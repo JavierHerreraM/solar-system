057c081053faec4242be9df55bea81570fe36ef4
javier <javiherreramad@gmail.com>
2021-02-03 20:05:21 -0600

    Change the way the nav comp hides the options in small screens

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 import './nav.scss';
 
 function NavBar() {
-    const isBigScreen = useMediaQuery({ query: '(min-device-width: 769px)' });
-
     return (
         <header className='navbar'>
             <h1 className='navbar-title'>Solar System</h1>
-            {/* If the screen is tablet portrait or below it don't show the options */}
-            {isBigScreen &&
-                <nav className='navbar-options'>
-                    <Link to='/' className="navbar-option" >Home</Link>
-                    <Link to='/planets:earth' className="navbar-option" >Planets</Link>
-                </nav>}
+            <nav className='navbar-options'>
+                <Link to='/' className="navbar-option" >Home</Link>
+                <Link to='/planets:earth' className="navbar-option" >Planets</Link>
+            </nav>
         </header>
     );
 }
